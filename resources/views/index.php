6bd87766107422cdf75cc97e160630a4a7c61ddb
Blaj3n <benceulrich@gmail.com>
2022-11-08 14:34:34 +0100

    index.php fájlok elérhetőségének módosítása

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/../css/style.css">
+    <link rel="stylesheet" href="/../css/konyvAdmin.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/../js/main.js" type="module"></script>
     <script
   src="https://code.jquery.com/jquery-3.6.1.js"
   integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
   crossorigin="anonymous"></script>
     <script src="app.js" type="module"></script>
-    <title>György Krisztián</title>
+    <title>Ulrich Bence</title>
 </head>
 <body>
     <main>
