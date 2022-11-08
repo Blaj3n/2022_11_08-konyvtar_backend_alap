f0b148bc5d933344e03938382911713973b1a5b1
Blaj3n <benceulrich@gmail.com>
2022-11-08 15:13:00 +0100

    app.css és app.js és bootstrap.js fájl áthelyezésre a public mappába

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html lang="hu">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/../css/style.css">
-    <link rel="stylesheet" href="/../css/konyvAdmin.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="/../js/main.js" type="module"></script>
-    <script
-  src="https://code.jquery.com/jquery-3.6.1.js"
-  integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
-  crossorigin="anonymous"></script>
-    <script src="app.js" type="module"></script>
-    <title>Ulrich Bence</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/../css/style.css">
+  <link rel="stylesheet" href="/../css/konyvAdmin.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="/../js/main.js" type="module"></script>
+  <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+  <script src="app.js" type="module"></script>
+  <title>Ulrich Bence</title>
 </head>
+
 <body>
-    <main>
-        <nav>
-          <ul>
-            <li id="public"><a href="#">Publikus</a></li>
-            <li id="admin"><a href="#">Admin</a></li>
-            <li id="kosar"><a href="#">Kosár</a></li>
-          </ul>
-        </nav>
-        <article>
-          
-        </article>
-    </main>
+  <main>
+    <nav>
+      <ul>
+        <li id="public"><a href="#">Publikus</a></li>
+        <li id="admin"><a href="#">Admin</a></li>
+        <li id="kosar"><a href="#">Kosár</a></li>
+      </ul>
+    </nav>
+    <article>
+
+    </article>
+  </main>
 </body>
+
 </html>
